8204ac95c2c2de70f1b8cab96eba153b730b3549
AzizaHalder <azizahalder9@gmail.com>
2022-12-07 23:03:17 +0100

    all iterations complete

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+let items = [];
+
 class SortedList {
-  constructor(items, length) {
-    this.items = items;
-    this.length = length;
+  constructor() {
+    this.items = [];
+    this.length = 0;
   }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    // this.length ++
+    this.items.sort((a, b) => a - b);
+    return (this.length = this.items.length);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length >= pos) {
+      return this.items.indexOf(pos);
+    } else {
+      throw new Error("OutOfBounds");
+    }
+    // could I also do this with charAt()?
+  }
 
-  max() {}
+  max() {
+    if (this.items.length == 0) throw new Error("OutOfBounds");
 
-  min() {}
+    // return this.items[this.items.length - 1];
+    return this.items.slice(-1);
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length == 0) throw new Error("OutOfBounds");
 
-  avg() {}
+    return this.items[0];
+  }
+
+  sum() {
+    if (this.items.length == 0) return 0;
+    return this.items.reduce((total, amount) => total + amount);
+  }
+
+  avg() {
+    if (this.items.length == 0) throw new Error("EmptySortedList");
+    return this.sum() / this.items.length;
+  }
 }
 
 module.exports = SortedList;
